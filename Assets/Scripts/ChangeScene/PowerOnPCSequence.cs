498cc4c43a1ce3de401835fd20cdb4817e07f679
CristianDC-Junkier <cristiandelgadoinformatica@gmail.com>
2026-05-17 16:28:20 +0200

    Eliminando Commit Anterior + Cambios
    Transformo mi Escena "Cristian" en la de la casa, aunque el perro aún no funciona, y faltan paredes para que no se caiga (estoy esperando que dani termine con el perro) también le falta lo de la comida.

---
+++ Assets/Scripts/ChangeScene/PowerOnPCSequence.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PowerOnPCSequence : MonoBehaviour
+{
+    [Header("Transición oscura")]
+    public Image darkScreen;
+
+    [Header("Elemento de seguimiento")]
+    public GameObject followUI;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip windowsOn;
+
+    [Header("Escena a cambiar")]
+    public Object nextScene;
+
+    void Start()
+    {
+        if (darkScreen != null) darkScreen.color = new Color(0, 0, 0, 0);
+        StartCoroutine(WaitTransition());
+    }
+
+    IEnumerator WaitTransition()
+    {
+        while (true)
+        {
+
+            if (
+                OVRInput.GetDown(OVRInput.Button.One) ||
+                OVRInput.GetDown(OVRInput.Button.Two) ||
+                OVRInput.GetDown(OVRInput.Button.Three) ||
+                OVRInput.GetDown(OVRInput.Button.Four) ||
+                OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.8f ||
+                OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.8f ||
+                OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.8f ||
+                OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0.8f
+            )
+            {
+                yield return StartCoroutine(ChangeTransition());
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator ChangeTransition()
+    {
+        float tiempo = 0;
+        float duracionFade = 1.5f;
+
+        // Fundido a negro
+        while (tiempo < duracionFade)
+        {
+            tiempo += Time.deltaTime;
+            if (darkScreen != null)
+                darkScreen.color = new Color(0, 0, 0, Mathf.Clamp01(tiempo / duracionFade));
+            yield return null;
+        }
+
+        foreach (Transform hijo in followUI.transform)
+        {
+            hijo.gameObject.SetActive(false);
+        }
+        audioSource.Stop();
+
+        // Música de transición final antes de cambiar escena
+        audioSource.clip = windowsOn;
+        audioSource.volume = 1f;
+        audioSource.Play();
+        yield return new WaitForSeconds(windowsOn.length);
+
+        SceneManager.LoadScene(nextScene.name);
+    }
+}
