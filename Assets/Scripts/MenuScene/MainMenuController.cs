147b5e55cce1f7584b80d538dd17f86e2e5dadd6
CristianDC-Junkier <cristiandelgadoinformatica@gmail.com>
2026-05-22 23:28:04 +0200

    Integración Parcial del Menú del PC.
    Faltan algunas mejoras y algunos fallos como:
    -Posición de la cámara.
    -Menú de opciones (el del brillo, no funciona bien)
    -Algunos botones no suenan al ser pulsados.
    -El salir no termina el juego
    
    El menú de pausa aun no lo toqué.
    
    También puse el HDRI en la escena de la casa, faltaría ponerlo en todas las que sea necesario (menú y parque)

---
+++ Assets/Scripts/MenuScene/MainMenuController.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("Transición oscura")]
+    public Image darkScreen;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip windowsOff;
+    public AudioClip doorSound;
+
+    [Header("Escena a cambiar")]
+    public Object nextScene; // Esto puede fallar al compilar el juego entero, sería mejor usar un string con el nombre de la escena
+
+    void Start()
+    {
+        if (darkScreen != null) darkScreen.color = new Color(0, 0, 0, 0);
+    }
+
+    public void ContinueGame()
+    {
+        StartCoroutine(ChangeTransition()); // Arrancamos la transición
+    }
+
+    IEnumerator ChangeTransition()
+    {
+        float tiempo = 0;
+        float duracionFade = 1.5f;
+
+        // Fundido a negro
+        while (tiempo < duracionFade)
+        {
+            tiempo += Time.deltaTime;
+            if (darkScreen != null)
+                darkScreen.color = new Color(0, 0, 0, Mathf.Clamp01(tiempo / duracionFade));
+            yield return null;
+        }
+
+        audioSource.Stop();
+
+        // Música de transición final antes de cambiar escena
+        audioSource.clip = windowsOff;
+        audioSource.volume = 1f;
+        audioSource.Play();
+        yield return new WaitForSeconds(windowsOff.length);
+
+        SceneManager.LoadScene(nextScene.name);
+    }
+
+    public void QuitGame()
+    {
+        StartCoroutine(Quitransition()); // Arrancamos la transición
+    }
+
+    IEnumerator Quitransition()
+    {
+        float tiempo = 0;
+        float duracionFade = 1.5f;
+
+        // Fundido a negro
+        while (tiempo < duracionFade)
+        {
+            tiempo += Time.deltaTime;
+            if (darkScreen != null)
+                darkScreen.color = new Color(0, 0, 0, Mathf.Clamp01(tiempo / duracionFade));
+            yield return null;
+        }
+
+        audioSource.Stop();
+
+        // Música de transición final antes de cambiar escena
+        audioSource.clip = doorSound;
+        audioSource.volume = 1f;
+        audioSource.Play();
+        yield return new WaitForSeconds(doorSound.length);
+
+        Application.Quit();
+    }
+}
